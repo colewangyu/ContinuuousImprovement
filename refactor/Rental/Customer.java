c501c5f8441d1f7bbc6aa1f2423e3176d1ff07ac
yu wang <yu wang>
2017-04-13 19:45:03 +0800

    Initial rental program.

---
+++ refactor/Rental/Customer.java
@@ -0,0 +1,67 @@
+package Rental;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+    private String _name;
+    private Vector<Rental> _rentals = new Vector<Rental>();
+
+    public Customer(String _name) {
+        this._name = _name;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public void addRental(Rental rental) {
+        this._rentals.addElement(rental);
+    }
+
+    /**
+     * 生成报表
+     *
+     * @return
+     */
+    public String statement() {
+        double totalAmount = 0;
+        int renterPoints = 0;
+        Enumeration<Rental> rentals = _rentals.elements();
+        String result = "Rental Record for " + getName() + "\n";
+        while (rentals.hasMoreElements()) {
+            double thisAmount = 0;
+            Rental each = rentals.nextElement();
+
+            // determine amounts for each line
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2)
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3)
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    break;
+            }
+
+            // add renter points
+            renterPoints++;
+            // add bonus for a two day new release rental
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) renterPoints++;
+
+            //show figures for this rental
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+            totalAmount += thisAmount;
+        }
+        // add footer lines
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(renterPoints) + " renter points.";
+        return result;
+    }
+}
