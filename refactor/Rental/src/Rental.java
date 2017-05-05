3ee7af24de5f35471ef8c4f9ccba1c03cdab7192
yu wang <yu wang>
2017-05-05 15:18:51 +0800

    Moved method from Rental to Movie.

--- refactor/Rental/src/Rental.java
+++ refactor/Rental/src/Rental.java
@@ -1,5 +1,5 @@
 public class Rental {
-    private Movie _movie;
+    Movie _movie;
     private int _daysRented;
 
     public Rental(Movie _movie, int _daysRented) {
@@ -15,31 +15,15 @@ public class Rental {
         return _daysRented;
     }
 
-    public double getThisAmount() {
-        // determine amounts for each line
-        double thisAmount = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (getDaysRented() > 2)
-                    thisAmount += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (getDaysRented() > 3)
-                    thisAmount += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
-    }
-
     public int getRenterPoints() {
         if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
             return 2;
         else
             return 1;
+    }
+
+    public double getThisAmount() {
+        // determine amounts for each line
+        return this._movie.getThisAmount(this._daysRented);
     }
 }
